9516a407eceffebf63caf9f79da70a89f5910d07
irynapavelchak <iryna.pavelchak@gmail.com>
2016-11-25 20:07:38 +0200

    Built_in task

---
+++ task_builtIN.py
@@ -0,0 +1,23 @@
+def divisors(n):
+    l = []
+    for i in range (0, n):
+        res = divmod(n, i)
+        if res[1] == 0:
+            l.append(res[0])
+    print l
+        
+
+
+
+
+if __name__ == '__main__':
+    q = True
+    while q == True:
+        n1 = input('Please input positive number: ')
+        if n1 <= 0:
+            print 'Please put positive value'
+        else:
+            q = False
+    divisors(n1)
+              
+              
