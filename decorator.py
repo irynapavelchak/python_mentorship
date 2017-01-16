93436f5e280805409dfbc9b5cb6c0509466bb1f5
irynapavelchak <iryna.pavelchak@gmail.com>
2017-01-16 15:23:02 +0200

    args, kwargs improvement

--- decorator.py
+++ decorator.py
@@ -1,11 +1,15 @@
 def decorator1(func):
-    def function1(a, b):
-        print "Arguments of function are:", a, b
-        return func(a, b)
+    def function1(*args, **kwargs):
+        print "Arguments of function are:"
+        for arg in args:
+            print arg
+        for key in kwargs:
+            print key, ":", kwargs[key]                
+        return func(*args, **kwargs)
     return function1
 
 @decorator1
-def get_arguments(a, b):
+def get_arguments(*args, **kwargs):
     return "some result"
 
 get_arguments("bbb1", "ffff1")
